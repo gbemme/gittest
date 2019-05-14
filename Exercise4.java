09765ec1bebeb4b839935bcfbec2c9c299b9a17a
Gbemisola N <gbemikp@gmail.com>
2019-05-14 16:41:29 +0100

    new class created

---
+++ Exercise4.java
@@ -0,0 +1,17 @@
+// program to determine oldest and youngest
+import java.util.Scanner;
+public class Exercise4{
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        System.out.println("enter first age");
+        int a = input.nextInt();
+        System.out.println("enter second age");
+        int b = input.nextInt();
+        if (a >=b){
+            System.out.println("oldest");
+        }
+        else;{
+            System.out.println("youngest");
+        }
+    }
+}
